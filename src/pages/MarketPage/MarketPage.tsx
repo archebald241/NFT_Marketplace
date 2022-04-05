f3f535d788cf4b7d703812f44d1f27feaba8868f
Oleg Kapitanov <archebald404@gmail.com>
2022-04-05 16:00:06 +0700

    add filtration "availability"

--- src/pages/MarketPage/MarketPage.tsx
+++ src/pages/MarketPage/MarketPage.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import './MarketPage.scss'
 import Card from '../../components/Card/Card';
 import {fetchProducts} from "../../api/products";
@@ -6,10 +6,24 @@ import {fetchProducts} from "../../api/products";
 
 const MarketPage = () => {
     const [products, setProducts] = useState<Array<any>>([])
+    const [isFiltered, serIsFiltered] = useState(false)
 
     const getProducts = async () => {
         const response = await fetchProducts()
         setProducts(response)
+    }
+
+    const filteredProducts = useMemo(() => {
+            if (isFiltered) {
+                return products.filter((product) => product['quantity_available'] > 0)
+            } else {
+                return products
+            }
+        },
+        [products, isFiltered])
+
+    const availabilityFilter = () => {
+        serIsFiltered(!isFiltered)
     }
 
     useEffect(() => {
@@ -20,8 +34,17 @@ const MarketPage = () => {
         <main>
             {products.length > 0
                 ? <div className={'cards'}>
+                    <div className={'card-navigation'}>
+                        <div className={''}>
+
+                        </div>
+                        <div className={'filter-by-quantity'}>
+                            <label htmlFor={'checkFilt'}>В наличии:</label>&nbsp;
+                            <input type="checkbox" id={'checkFilt'} name={'checkFilt'} checked={isFiltered} onChange={availabilityFilter}/>
+                        </div>
+                    </div>
                     <div className={'card-list'}>
-                        {products
+                        {filteredProducts
                             .map((product) =>
                                 <Card author={product['created_by']['display_name']}
                                       name={product['name']}
